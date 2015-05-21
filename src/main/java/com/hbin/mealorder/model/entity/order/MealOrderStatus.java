df62b3e4ca549b77c272a32cf259965192c562c5
linhaobin8898 <864037833@qq.com>
2015-05-21 14:59:16 +0800

    订单状态

--- src/main/java/com/hbin/mealorder/model/entity/order/MealOrderStatus.java
+++ src/main/java/com/hbin/mealorder/model/entity/order/MealOrderStatus.java
@@ -2,4 +2,18 @@ package com.hbin.mealorder.model.entity.order;
 
 public enum MealOrderStatus {
 
+	正在点餐(0),
+
+	结束点餐(1);
+
+	private int code;
+
+	MealOrderStatus(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
 }
