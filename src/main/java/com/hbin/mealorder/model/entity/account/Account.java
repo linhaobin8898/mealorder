1753c188b09757d722d5cfb05dab985a439ee7b4
linhaobin8898 <864037833@qq.com>
2015-05-21 13:45:58 +0800

    init

---
+++ src/main/java/com/hbin/mealorder/model/entity/account/Account.java
@@ -0,0 +1,57 @@
+package com.hbin.mealorder.model.entity.account;
+
+import com.lifesense.framework.mybatis.entity.id.UUIDEntity;
+
+@SuppressWarnings("serial")
+public class Account implements UUIDEntity {
+
+	private String id;
+
+	private String openId;
+
+	private String nickname;
+
+	private String remarkName;
+
+	private String headimg;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getOpenId() {
+		return openId;
+	}
+
+	public void setOpenId(String openId) {
+		this.openId = openId;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getRemarkName() {
+		return remarkName;
+	}
+
+	public void setRemarkName(String remarkName) {
+		this.remarkName = remarkName;
+	}
+
+	public String getHeadimg() {
+		return headimg;
+	}
+
+	public void setHeadimg(String headimg) {
+		this.headimg = headimg;
+	}
+}
---
+++ src/main/java/com/hbin/mealorder/model/entity/meal/Meal.java
@@ -0,0 +1,38 @@
+package com.hbin.mealorder.model.entity.meal;
+
+import com.lifesense.framework.mybatis.entity.id.UUIDEntity;
+
+@SuppressWarnings("serial")
+public class Meal implements UUIDEntity {
+
+	private String id;
+
+	private String name;
+
+	private Double price;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+}
